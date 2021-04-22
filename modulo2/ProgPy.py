98bcdbd3860c54c16a5b8bfbe28c3942ee4ccdc4
Omar García <nando943105@hotmail.com>
2021-04-22 14:46:06 +0200

    Practica con Python

---
+++ modulo2/ProgPy.py
@@ -0,0 +1,24 @@
+numero1 = 10
+numero2 = 30
+numero3 = 50
+saludo1 = "Buongiorno!!!"
+saludo2 = "Bonjour!!!"
+saludo3 = "Good Morning"
+
+print(numero1)
+print(saludo1)
+
+print(type(numero1))
+print(type(saludo1))
+
+print(numero2)
+print(saludo2)
+
+print(type(numero2))
+print(type(saludo2))
+
+print(numero3)
+print(saludo3)
+
+print(type(numero3))
+print(type(saludo3))
