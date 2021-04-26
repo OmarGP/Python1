70519bf1d9d04b7fc606725769c22db1bec8083d
Omar García <nando943105@hotmail.com>
2021-04-26 19:55:43 +0200

    Actualización #3

--- modulo2/Fechas.py
+++ modulo2/Fechas.py
@@ -7,6 +7,26 @@ print("Fecha1: ", dt1.strftime("%A %d-%m-%Y"))
 print("Fecha2: ", dt2)
 print("Año: ", dt2.year)
 print("Month: ", dt2.month)
+print("Día: ", dt2.day)
+print(f"Hora: {dt2.hour}:{dt2.minute}")
+
+print("===============================================")
+fecha32 ="10-11-2018"
+obj = datetime.strptime(fecha32, "%d-%m-%Y").date()
+print(obj)
+print(f"{obj.day}-{obj.month}-{obj.year}")
+
+
+print("===============================================")
+
+print(dt2.strftime("%A, %d %b %Y"))
+
+
+
+
+
+
+print("===============================================")
 
 fecha = input("Escribe tu fecha de nacimiento: ")
 dt3 = datetime.strptime(fecha, "%d-%m-%Y").date()
@@ -14,3 +34,5 @@ años = dt1.year - dt3.year
 
 print("Fecha3: ", dt3)
 print(f"Tienes {años} años.")
+
+
