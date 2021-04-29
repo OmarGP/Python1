842d8e70b5bc32d034168dd10a578d072044ae2e
Omar García <nando943105@hotmail.com>
2021-04-29 20:56:13 +0200

    Subir

--- modulo2/Módulos.py
+++ modulo2/Módulos.py
@@ -1,7 +1,41 @@
-#from modulo import alumno
-#alumno = modulo.Alumno("Omar", "García", "Prado")
-#print(f"{alumno.getNombreCompleto()}")
+class Cliente:
+    Identificador = None
+    Nombre = None
+    Apellidos = None
+    Genero = None
+    Pais = None
+    FechaNacimiento = None
 
-import modulo
-alumno = modulo.Alumno("Omar", "García", "Prado")
-print(f"{alumno.getNombreCompleto()}")
+    def __init__(self, id, nombre, apellidos) -> None:
+        self.Identificador = id
+        self.Nombre = nombre
+        self.Apellidos = apellidos
+    
+    
+clientes = []
+path = "modulo2\\fichero.txt"
+file = open(path)
+
+for linea in (file.readlines()):
+    data = linea.split(",")
+    if (data[0].isdigit() == True):
+        #clientes.append(Cliente(data[7], data[1], data[2]))
+        cliente = Cliente(data[7], data[1], data[2])
+        clientes.append(cliente)
+
+
+file.close
+print(f"{len(clientes)} clientes importados.")
+
+#for c in clientes:
+    #print(c.Nombre)
+
+while (True):
+    posicion = int(input("Dime una posición: "))
+    print(f"{clientes[posicion].Nombre} {clientes[posicion].Apellidos}")
+
+    break
+
+print(type(clientes))
+
+print("FIN")
